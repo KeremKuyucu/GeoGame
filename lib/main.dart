aca8181c0c13b0298a6c151926d7e35632d6f504
Kerem Kuyucu <kkuyucu2254@gmail.com>
2025-12-27 00:22:17 +0300

    feat: Initialize the Flutter app with Supabase, add a user profile screen, and update the installer version.

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,8 @@ class GeoGame extends State<Geogame> {
 /*
 flutter pub run flutter_launcher_icons:main
 
+flutter build web
 flutter build apk --release --split-per-abi
 flutter build windows
+
 */
